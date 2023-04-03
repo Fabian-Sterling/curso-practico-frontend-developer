4c71b0f614f6fee8a7f4e6005e623156f465ede2
Fabian Sanchez <alfasas92@hotmail.com>
2023-04-03 14:59:28 -0500

    Se realiza la division de los archivos, HTML y CSS. Para crear la fusion de JavasCrip y darle dinamismo a la pag. Se realiaza la fusion del menu Destock y mobile en el nav. y de productos, en los productos se realiza la maquetacion de HTML desde JavasCrip

--- main.js
+++ main.js
@@ -1,8 +1,123 @@
 const MenuEmail = document.querySelector('.navbar-email');
 const DesktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const cardsContainer = document.querySelector('.cards-container');
 
 MenuEmail.addEventListener('click' , toggleDesktopMenu);
+menuHamIcon.addEventListener('click' , toggleMobileMenu);
+menuCarritoIcon.addEventListener('click' , toggleCarritoAside);
 
 function toggleDesktopMenu (){
+    const isCarritoAsideClosed = aside.classList.contains ('inactive')
+
+    if (!isCarritoAsideClosed){
+        aside.classList.add('inactive')
+    }
+
     DesktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu (){
+    const isCarritoAsideClosed = aside.classList.contains ('inactive')
+
+    if (!isCarritoAsideClosed){
+        aside.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains ('inactive')
+    
+    const isDesktopMenuClosed = DesktopMenu.classList.contains ('inactive')
+
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+
+    if (!isDesktopMenuClosed){
+        DesktopMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({
+    name: 'Camisa',
+    price: 20,
+    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({
+    name: 'Celular',
+    price: 400,
+    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({
+    name: 'Auriculares',
+    price: 100,
+    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({
+    name: 'Reloj',
+    price: 500,
+    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+function renderProducts(arr){
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const produtImg = document.createElement('img');
+        produtImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute ('src', './Icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCard);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(produtImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
